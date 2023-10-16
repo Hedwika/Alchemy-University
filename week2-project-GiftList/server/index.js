e519e45f58821377c6c1060998a3ebd9843ea668
hedwika <pajerova.hedvika@gmail.com>
2023-10-16 15:06:45 -0700

    index.js server side updated

--- week2-project-GiftList/server/index.js
+++ week2-project-GiftList/server/index.js
@@ -6,16 +6,22 @@ const port = 1225;
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
+// DONE: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = "14a063217458e0bf11435c89ad74d22074fe441c9bdaf3d4c9d6238d84c8cd90";
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  // DONE: prove that a name is in the list 
+  let isInTheList = false;
+  const verification = verifyProof(proof, name, MERKLE_ROOT);
+
+  if (verification) {
+    isInTheList = true;
+  }
+
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
