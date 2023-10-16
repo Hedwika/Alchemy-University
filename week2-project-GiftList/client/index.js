6c50f1c99a7936c25321b35d420ecf8e859a6ecd
hedwika <pajerova.hedvika@gmail.com>
2023-10-16 12:38:12 -0700

    Index.js client side first steps

--- week2-project-GiftList/client/index.js
+++ week2-project-GiftList/client/index.js
@@ -1,17 +1,31 @@
 const axios = require('axios');
+const readline = require('readline');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  rl.question('Please enter your name: ', async (name) => {
+    try {
+      const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+        // TODO: add request body parameters here!
+        name
+      });
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+      console.log({ gift });
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+    } finally {
+      rl.close();
+    }
   });
-
-  console.log({ gift });
 }
 
 main();
